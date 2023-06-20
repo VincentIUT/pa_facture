2eee6986dbd417be605d4b680c1d746bdd16475e
Vincent <v.bouchet78@gmail.com>
2023-06-20 16:00:46 +0200

    update local

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import  Axios  from "axios"
 
 const api = Axios.create({baseURL: "http://10.33.5.131:5000/", headers: {'Content-Type': 'multipart/form-data' } })
+//const api = Axios.create({baseURL: "http://127.0.0.1:5000/", headers: {'Content-Type': 'multipart/form-data' } })
 
 export default api;
